9ee02ad34d05b2d2a76053714ed1ca2c77f13d26
Rechner Fox <jkltechinc@gmail.com>
2016-02-09 02:21:38 -0500

    spaaaaaaces

--- php/functions.php
+++ php/functions.php
@@ -4,145 +4,145 @@ define('SALT_LENGTH', 32);
 function sec_session_start() {
         $session_name = 'sec_session_id'; // Set a custom session name
         $secure = false; // Set to true if using https.
-        $httponly = true; // This stops javascript being able to access the session id. 
- 
-        ini_set('session.use_only_cookies', 1); // Forces sessions to only use cookies. 
+        $httponly = true; // This stops javascript being able to access the session id.
+
+        ini_set('session.use_only_cookies', 1); // Forces sessions to only use cookies.
         $cookieParams = session_get_cookie_params(); // Gets current cookies params.
-        session_set_cookie_params($cookieParams["lifetime"], $cookieParams["path"], $cookieParams["domain"], $secure, $httponly); 
+        session_set_cookie_params($cookieParams["lifetime"], $cookieParams["path"], $cookieParams["domain"], $secure, $httponly);
         session_name($session_name); // Sets the session name to the one set above.
         session_start(); // Start the php session
-        session_regenerate_id(true); // regenerated the session, delete the old one.     
+        session_regenerate_id(true); // regenerated the session, delete the old one.
 }
 
 function generate_hash($password, $salt = null) {
-	if ($salt === null) {												//if there is no salt argument
-		$salt = substr(md5(uniqid(rand(), true)), 0, SALT_LENGTH);		//generate a new salt
-	} else {															//otherwise
-		$salt = substr($salt, 0, SALT_LENGTH);							//use the salt provided
-	}
-	return $salt . hash('sha512', $salt . $password);					//return the sha512'd password appended to the end of the hash
+    if ($salt === null) {                                               //if there is no salt argument
+        $salt = substr(md5(uniqid(rand(), true)), 0, SALT_LENGTH);      //generate a new salt
+    } else {                                                            //otherwise
+        $salt = substr($salt, 0, SALT_LENGTH);                          //use the salt provided
+    }
+    return $salt . hash('sha512', $salt . $password);                   //return the sha512'd password appended to the end of the hash
 }
 
 function login($username, $password, $pdo) {
-	if ($query = $pdo->prepare("SELECT uid, username, password FROM accounts WHERE username = ? LIMIT 1")) { 
-		$query->bindValue(1, $username); // Bind "$username" to parameter.
-		
-		$query->execute(); // Execute the prepared query.
-		$result = $query->fetch();
+    if ($query = $pdo->prepare("SELECT uid, username, password FROM accounts WHERE username = ? LIMIT 1")) {
+        $query->bindValue(1, $username); // Bind "$username" to parameter.
 
-		$user_id = $result['uid'];
-		$username = $result['username'];
-		$storedpass = $result['password'];
+        $query->execute(); // Execute the prepared query.
+        $result = $query->fetch();
 
-		$storedsalt = substr($storedpass, 0, 32); // break salt from stored hash
+        $user_id = $result['uid'];
+        $username = $result['username'];
+        $storedpass = $result['password'];
 
-		$password = generate_hash($password, $storedsalt); // hash the attempted password with the unique salt from database.
+        $storedsalt = substr($storedpass, 0, 32); // break salt from stored hash
 
-		if($result) { // If the user exists
-			if($storedpass == $password) { // Check if the password in the database matches the password the user submitted. 
-				// Password is correct!
-				$ip_address = $_SERVER['REMOTE_ADDR']; // Get the IP address of the user. 
-				$user_browser = $_SERVER['HTTP_USER_AGENT']; // Get the user-agent string of the user.
+        $password = generate_hash($password, $storedsalt); // hash the attempted password with the unique salt from database.
 
-				$user_id = preg_replace("/[^0-9]+/", "", $user_id); // XSS protection as we might print this value
-				$_SESSION['user_id'] = $user_id; 
-				$username = preg_replace("/[^a-zA-Z0-9_\-]+/", "", $username); // XSS protection as we might print this value
-				$_SESSION['username'] = $username;
-				$_SESSION['login_string'] = hash('sha512', $password.$ip_address.$user_browser);
-				// Login successful.
-				return true;    
-			}
-		}
-	} else {
-		// No user exists. 
-		return false;
-	}
+        if($result) { // If the user exists
+            if($storedpass == $password) { // Check if the password in the database matches the password the user submitted.
+                // Password is correct!
+                $ip_address = $_SERVER['REMOTE_ADDR']; // Get the IP address of the user.
+                $user_browser = $_SERVER['HTTP_USER_AGENT']; // Get the user-agent string of the user.
+
+                $user_id = preg_replace("/[^0-9]+/", "", $user_id); // XSS protection as we might print this value
+                $_SESSION['user_id'] = $user_id;
+                $username = preg_replace("/[^a-zA-Z0-9_\-]+/", "", $username); // XSS protection as we might print this value
+                $_SESSION['username'] = $username;
+                $_SESSION['login_string'] = hash('sha512', $password.$ip_address.$user_browser);
+                // Login successful.
+                return true;
+            }
+        }
+    } else {
+        // No user exists.
+        return false;
+    }
 }
 
 function login_check($pdo) {
-	// Check if all session variables are set
-	if(isset($_SESSION['user_id'], $_SESSION['username'], $_SESSION['login_string'])) {
-		$user_id = $_SESSION['user_id'];
-		$login_string = $_SESSION['login_string'];
-		$username = $_SESSION['username'];
-		$ip_address = $_SERVER['REMOTE_ADDR']; // Get the IP address of the user. 
-		$user_browser = $_SERVER['HTTP_USER_AGENT']; // Get the user-agent string of the user.
+    // Check if all session variables are set
+    if(isset($_SESSION['user_id'], $_SESSION['username'], $_SESSION['login_string'])) {
+        $user_id = $_SESSION['user_id'];
+        $login_string = $_SESSION['login_string'];
+        $username = $_SESSION['username'];
+        $ip_address = $_SERVER['REMOTE_ADDR']; // Get the IP address of the user.
+        $user_browser = $_SERVER['HTTP_USER_AGENT']; // Get the user-agent string of the user.
 
-		if($query = $pdo->prepare("SELECT password FROM accounts WHERE uid = ? LIMIT 1")) { 
-			$query->bindValue(1, $user_id); // Bind "$user_id" to parameter.
-			$query->execute(); // Execute the prepared query.
-			$result = $query->fetch();
- 
-			if($result) { // If the user exists
-				$password = $result['password']; // get variables from result.
-				$login_check = hash('sha512', $password.$ip_address.$user_browser);
-				if($login_check == $login_string) {
-					// Logged In!!!!
-					return true;
-				} else {
-					// Not logged in
-					return false;
-				}
-			} else {
-				// Not logged in
-				return false;
-			}
-		} else {
-			// Not logged in
-			return false;
-		}
-	} else {
-		// Not logged in
-		return false;
-	}
+        if($query = $pdo->prepare("SELECT password FROM accounts WHERE uid = ? LIMIT 1")) {
+            $query->bindValue(1, $user_id); // Bind "$user_id" to parameter.
+            $query->execute(); // Execute the prepared query.
+            $result = $query->fetch();
+
+            if($result) { // If the user exists
+                $password = $result['password']; // get variables from result.
+                $login_check = hash('sha512', $password.$ip_address.$user_browser);
+                if($login_check == $login_string) {
+                    // Logged In!!!!
+                    return true;
+                } else {
+                    // Not logged in
+                    return false;
+                }
+            } else {
+                // Not logged in
+                return false;
+            }
+        } else {
+            // Not logged in
+            return false;
+        }
+    } else {
+        // Not logged in
+        return false;
+    }
 }
 
 function show_notices()
 {
-	if ($_SESSION['notices'])
-	{
-		foreach ($_SESSION['notices'] as $notice)
-		{
-			echo "<div class='alert alert-{$notice['type']} alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>{$notice['message']}</div>";
-		}
-		unset($_SESSION['notices']);
-	}
+    if (in_array('notices', $_SESSION) and $_SESSION['notices'])
+    {
+        foreach ($_SESSION['notices'] as $notice)
+        {
+            echo "<div class='alert alert-{$notice['type']} alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>{$notice['message']}</div>";
+        }
+        unset($_SESSION['notices']);
+    }
 }
 
 function show_message($type, $message)
 {
-	$_SESSION['notices'][] = array("type" => $type, "message" => $message);
+    $_SESSION['notices'][] = array("type" => $type, "message" => $message);
 }
 
 function send_email($to, $subject, $message)
 {
-	require_once "Mail.php";
- 
-	$from = "Hamfurs <noreply@hamfurs.org>";
-	$body = $message;
- 
-	$host = "ssl://smtp.zoho.com";
-	$port = "465";
-	$username = "noreply@hamfurs.org";
-	$password = "JuRl08jr2ld5EbSS";
- 
-	$headers = array ('From' => $from,
-	  'To' => $to,
-	  'Subject' => $subject);
-	$smtp = Mail::factory('smtp',
-	  array ('host' => $host,
-		'port' => $port,
-		'auth' => true,
-		'username' => $username,
-		'password' => $password));
- 
-	$mail = $smtp->send($to, $headers, $body);
- 
-	if (PEAR::isError($mail)) {
-	 // showMessage("danger", $mail->getMessage());
-	} else {
-	  //showMessage("success", "Message successfully sent!");
-	}
+    require_once "Mail.php";
+
+    $from = "Hamfurs <noreply@hamfurs.org>";
+    $body = $message;
+
+    $host = "ssl://smtp.zoho.com";
+    $port = "465";
+    $username = "noreply@hamfurs.org";
+    $password = "JuRl08jr2ld5EbSS";
+
+    $headers = array ('From' => $from,
+      'To' => $to,
+      'Subject' => $subject);
+    $smtp = Mail::factory('smtp',
+      array ('host' => $host,
+        'port' => $port,
+        'auth' => true,
+        'username' => $username,
+        'password' => $password));
+
+    $mail = $smtp->send($to, $headers, $body);
+
+    if (PEAR::isError($mail)) {
+     // showMessage("danger", $mail->getMessage());
+    } else {
+      //showMessage("success", "Message successfully sent!");
+    }
 }
 
 
@@ -184,7 +184,7 @@ function generate_ticket($name, $code, $current=1, $seats=1, $outfile='') {
 
 class CSVException extends Exception { }
 
-function process_csv($csv_string) {
+function process_csv($acsv_string) {
   // Process the CSV into an array of associative arrays, using
   // the first row headers as keys.
   $rows = array_map('str_getcsv', file($csv_string));
@@ -193,7 +193,7 @@ function process_csv($csv_string) {
   do {
     $header = array_shift($rows);
   } while ((sizeof($header) > 1) or ($array[1] === ''));
-  
+
   if (!(in_array('Name', $header, true) and
         in_array('Spaces', $header, true) and
         in_array('Email', $header, true) and
